39e777635a228db3d883e1e81a992665891cd9ae
Jon Landers <jlanders@abstractmediaco.com>
2016-08-17 16:31:26 -0500

    higher order component tutorial

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super Special Secret Recipe
+      <ul>
+        <li>Fish</li>
+        <li>Diamonds</li>
+        <li>Gold Flakes</li>
+      </ul>
+    </div>
+    );
+};
